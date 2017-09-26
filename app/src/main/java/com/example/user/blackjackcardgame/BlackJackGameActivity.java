f349012ec7ba498b9bcd66cc8027d594241b17e7
Iain Paterson <iain.paterson.cc@gmail.com>
2017-09-26 14:36:08 +0100

    Add player one name to game activity

--- app/src/main/java/com/example/user/blackjackcardgame/BlackJackGameActivity.java
+++ app/src/main/java/com/example/user/blackjackcardgame/BlackJackGameActivity.java
@@ -1,13 +1,26 @@
 package com.example.user.blackjackcardgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BlackJackGameActivity extends AppCompatActivity {
 
+    TextView playerOneText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_black_jack_game);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_black_jack_game );
+
+        playerOneText = ( TextView ) findViewById( R.id.player_one_name_text );
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        String playerOneName = extras.getString( "playerOneName" );
+
+        playerOneText.setText( playerOneName );
     }
 }
--- app/src/main/java/com/example/user/blackjackcardgame/BlackJackPlayerActivity.java
+++ app/src/main/java/com/example/user/blackjackcardgame/BlackJackPlayerActivity.java
@@ -5,21 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class BlackJackPlayerActivity extends AppCompatActivity {
 
     Button startGameButton;
+    EditText playerOneText;
+    EditText playerTwoText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_black_jack_player);
 
-        startGameButton = (Button) findViewById(R.id.start_game_button);
+        startGameButton = ( Button ) findViewById(R.id.start_game_button);
+        playerOneText = ( EditText ) findViewById(R.id.player_one_name);
+        playerTwoText = ( EditText ) findViewById(R.id.player_two_name);
     }
 
     public void onStartGameButtonClicked(View Button) {
         Intent intent = new Intent(this, BlackJackGameActivity.class);
+
+        String playerOneName = playerOneText.getText().toString();
+        intent.putExtra( "playerOneName", playerOneName);
+        String playerTwoName = playerTwoText.getText().toString();
+        intent.putExtra( "playerTwoName", playerTwoName );
         startActivity( intent );
     }
 }
